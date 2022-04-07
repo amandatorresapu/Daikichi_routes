5414c4afc90ec337bedcb6de809d343db72e3537
Amanda Torres <69279617+amandatorresapu@users.noreply.github.com>
2022-04-07 15:54:35 -0700

    Add files via upload

---
+++ daikichiroutes/src/main/java/com/codingdojo/daikichiroutes/HomeController.java
@@ -0,0 +1,24 @@
+package com.codingdojo.daikichiroutes;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/daikichi")
+public class HomeController {
+	
+	@RequestMapping("")
+	public String welcome() {
+		return "welcome";
+	}
+	
+	@RequestMapping("/today")
+	public String today() {
+		return "Today you will find luck in all your endeavors!";
+	}
+	
+	@RequestMapping("/tomorrow")
+	public String tomorrow() {
+		return "goodluck tomorrow!";
+	}
+}
